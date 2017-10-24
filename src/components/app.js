b14fdee826381fd3e24268d31731095b899b1ba5
aaqibkhaan <aaqibrashid@hotmail.com>
2017-10-24 16:59:23 +0100

    component added

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import FetchCountries from './fetch_country.js'
+
+
+export default class App extends Component {
+
+	render() {
+
+		return(
+		<div>
+		<FetchCountries />
+		</div>
+		);
+	}
+
+
+}
---
+++ src/components/fetch_country.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+export default class fetchCountries extends Component {
+
+
+
+	render(){
+
+		return(
+			<div>
+				<h3>Fetching Countries Data</h3>
+			</div>
+			);
+
+	}
+}
