d310c484d9a6a4f039b4a3c9f16b0ef3d99474ce
aaqibkhaan <aaqibrashid@hotmail.com>
2017-10-26 21:28:11 +0100

    Component added and wired with redux

--- src/components/fetch_country.js
+++ src/components/fetch_country.js
@@ -5,14 +5,41 @@ import { fetchCountries } from '../actions';
 
 
 class FetchCountries extends Component {
+	componentDidMount(){
+		this.props.fetchCountries();
+	}
+/*
+	renderCountries() {
+		return _.map(this.props.countries , country => {
+			return (
+					<li className="list-group-item">
+							{country.capital}
+					</li>
+				);
+
+		});
 
 
-	render(){
+	}*/
 
-		return (
-			<div> FetchCountries Component</div>);
+	render() {
+		console.log(this.props.countries);
+
+		return(
+
+			<div>
+				<h3>Fetching Countries Data</h3>
+				<ul className="list-group">
+				</ul>
+			</div>
+			);
+
 	}
 }
 
+function mapStateToProps (state) {
+	return { countries: state.countries };
+}
 
-export default connect(null, { fetchCountries })(FetchCountries);
+
+export default connect (mapStateToProps, { fetchCountries })(FetchCountries);
