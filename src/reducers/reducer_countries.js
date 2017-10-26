8d10230bbb982e6631e1ad6b803525bbd2f5b7ef
aaqibkhaan <aaqibrashid@hotmail.com>
2017-10-26 21:29:28 +0100

    reducers and combined reducer added

--- src/reducers/reducer_countries.js
+++ src/reducers/reducer_countries.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
-import { FETCH_COUNTRIES} from '../actions';
+import { FETCH_COUNTRIES } from '../actions';
 
-export default function(state = {} , action) {
+export default function (state = {} , action) {
 	switch(action.type){
 		
 		case FETCH_COUNTRIES:
